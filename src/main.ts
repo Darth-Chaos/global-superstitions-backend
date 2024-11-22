1088b4b49342130781c6069052c76e8d5f69ebc8
DarthChaos <oniricmemories@gmail.com>
2024-11-22 17:25:04 -0500

    fix: Added front-end client to cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: 'http://localhost:5173', // O usa '*' para permitir todos los orígenes
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
